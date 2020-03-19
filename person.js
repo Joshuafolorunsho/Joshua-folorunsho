c5e1aabf8b4ce7387e8d388d1a8f938536ed3707
Joshua Folorunsho <joshuafolorunsho01@gmail.com>
2020-03-19 17:24:47 +0100

    added my details

---
+++ person.js
@@ -0,0 +1,11 @@
+const person = {
+   name: 'Joshua Folorunsho',
+   email: 'Joshuafolorunsho01@gmail.com',
+   slackUsername: 'Joshuafolorunsho',
+   track: 'Frontend',
+   coursesRegisteredFor: [
+      'HTML',
+      'CSS',
+      'JS'
+   ]
+}
